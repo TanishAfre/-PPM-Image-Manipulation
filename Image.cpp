08b9f47fce152cec9344afb4240b3b5ed17ad5fd
tanish <tanish.afre@gmail.com>
2022-04-09 14:38:47 +0100

    savePPM working Completely

--- Image.cpp
+++ Image.cpp
@@ -70,6 +70,27 @@ bool Image::loadRaw(string filename)
 }
 bool Image::savePPM(string filename)
 {
+    if (this->w == 0 || this->h == 0) {return false;}
+    std::ofstream ofs;
+    try {
+        ofs.open(filename, std::ios::binary); // need to spec. binary mode for Windows users
+        if (ofs.fail()) return false;
+        ofs << "P6\n"<< this->w << " " << this->h << "\n255\n";
+        unsigned char r, g, b;
+        // loop over each pixel in the image, clamp and convert to byte format
+        for (int i = 0; i < this->w * this->h; ++i) {
+            r = static_cast<unsigned char>(std::min(1.f, (float)this->pixels[i].r)* this->pixels[i].r);
+            g = static_cast<unsigned char>(std::min(1.f, (float)this->pixels[i].g)* this->pixels[i].g);
+            b = static_cast<unsigned char>(std::min(1.f, (float)this->pixels[i].b)* this->pixels[i].b);
+            ofs << r << g << b;
+        }
+        ofs.close();
+        return true;
+    }
+    catch (const char *err) {
+        fprintf(stderr, "%s\n", err);
+        ofs.close();
+    }
     return false;
 }
 
